e3fb7480889dcd26168cb21ad3268d4922a89c1e
Xavi-Dev <xjohndev@outlook.com>
2022-03-06 01:16:07 -0500

    Create employee data and pull employee list successfully. Moving onto finishing api calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 
@@ -12,7 +16,9 @@ import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashbo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeModel } from './models/employee-model';
+
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
-
-  constructor() { }
+  formValue !: FormGroup;
+  employeeModelObj: EmployeeModel = new EmployeeModel();
+  constructor(private formBuilder: FormBuilder, private api: ApiService) { }
 
   ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phoneNumber: [''],
+      salary: ['']
+    })
+  }
+
+  postEmployeeInfo() {
+    this.employeeModelObj.firstName = this.formValue.value.firstName;
+    this.employeeModelObj.lastName = this.formValue.value.lastName;
+    this.employeeModelObj.email = this.formValue.value.email;
+    this.employeeModelObj.phoneNumber = this.formValue.value.phoneNumber;
+    this.employeeModelObj.salary = this.formValue.value.salary;
+
+    this.api.postEmployee(this.employeeModelObj)
+    .subscribe(res => {
+      console.log(res);
+      alert("Employee Added Successfully")
+    },
+    err=>{
+      alert("Something went wrong!")
+    }
+    )
   }
 
 }
