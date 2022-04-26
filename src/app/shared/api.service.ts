2350d5c82797efeb6639a6da31e0d7ef5b10e073
Xavi-Dev <xjohndev@outlook.com>
2022-04-25 20:23:13 -0400

    Api link update

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -10,25 +10,25 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   postEmployee(data: Employee) {
-    return this.http.post<Employee>("http://localhost:3000/posts", data)
+    return this.http.post<Employee>("http://localhost:3000/employees", data)
     .pipe(map((res:any) =>{
       return res;
     }))
   }
 
   getEmployees() {
-    return this.http.get("http://localhost:3000/posts")
+    return this.http.get("http://localhost:3000/employees")
     .pipe(map((res:any) =>{
       return res;
     }))
   }
 
   editEmployee(data: Employee){
-    return this.http.put("http://localhost:3000/posts/", data);
+    return this.http.put("http://localhost:3000/employees/", data);
   }
 
   deleteEmployee(id: string){
-    return this.http.delete("http://localhost:3000/posts/" + id)
+    return this.http.delete("http://localhost:3000/employees/" + id)
   }
 }
 
